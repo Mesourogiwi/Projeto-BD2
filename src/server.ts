df2b42e79d252bba2671421872b5eabdcdb710b8
Lucas Quintas Honorato <lucasquintas@hotmail.com>
2022-02-06 16:22:11 -0300

    add CORS and port heroku

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from "express"
 import './database'
 import { routes } from "./routes";
 import 'dotenv/config'
+import cors from "cors"
 
 const app = express()
+app.use(cors())
 app.use(express.json());
 
 app.use(routes)
 
-app.listen('3000', () => console.log('funcionando'))
+app.listen(process.env.PORT || 3000, () => console.log('funcionando'))
