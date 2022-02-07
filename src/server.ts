75074f5f305f1513927d89c9cbfcc27e9c8c805c
Lucas Quintas Honorato <lucasquintas@hotmail.com>
2022-02-07 15:23:26 -0300

    add docs

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,14 @@ import './database'
 import { routes } from "./routes";
 import 'dotenv/config'
 import cors from "cors"
+import swaggerUi from "swagger-ui-express"
+import swaggerDocs from './swagger.json'
 
 const app = express()
 app.use(cors())
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.use(routes)
 
